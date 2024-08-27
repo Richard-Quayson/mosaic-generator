8b869ac2fce63300ef38704e10667dead0ca9ea4
Richard Quayson <richard.quayson@ashesi.edu.gh>
2024-08-27 23:36:44 +0000

    refactor: Add create_mosaic function which creates a Mosaic object and then draw coloured squares given a colour list

--- script/main.py
+++ script/main.py
@@ -1,4 +1,9 @@
-def main():
+from mosaic import Mosaic
 
-    
-    pass
+
+def create_mosaic(colour_list: list[list]) -> None:
+    mosaic = Mosaic(colour_list=colour_list)
+    mosaic.turtle.speed(0)
+    mosaic.draw_squares()
+    mosaic.turtle.hideturtle()
+    mosaic.turtle.done()
